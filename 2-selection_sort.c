4a3773e1ebe614b5920b3de78451ed585f98472a
scaarifintl <scaarifintl@gmail.com>
2022-11-04 10:23:31 +0300

    implement insertion sort and replace printf with print_array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,54 @@
 #include "sort.h"
 
+/**
+ * smallest - returns the position (index)
+ * of the smallest integer in an array starting
+ * at index, pos
+ * @arr: the array in which to look
+ * @pos: the starting lookup index
+ * @size: the size of the array
+ * Return: an integer index (position)
+ */
+int smallest(int *arr, int pos, int size)
+{
+	int i, min = arr[pos], min_pos = pos;
+
+	for (i = pos + 1; i < size; i++)
+	{
+		if (min > arr[i])
+		{
+			min = arr[i];
+			min_pos = i;
+		}
+	}
+	/*printf("min: %d at [%d]\n", min, min_pos);*/
+	return (min_pos);
+}
 
 /**
- *
- *
+ * selection_sort - sorts an array of integers in ascending
+ * order using selection sort algorithm
+ * (continuously swap the smallest value with the net unsorted
+ * element in array)
+ * @arr: the array of integers to sort
+ * @size_: number of elements in array
+ * Return: Nothing
  */
-void selection_sort(int *array, size_t size)
+void selection_sort(int *arr, size_t size_)
 {
-	int min_value, start_value, min_index;
-	size_t i, j, k;
-
+	int i, min_pos, temp, size = (int)size_;
 
 	for (i = 0; i < size; i++)
 	{
-		start_value = min_value = array[i];
-		min_index = i;
-		for (j = i; j < size ; j++)
+		min_pos = smallest(arr, i, size);
+		/* swap if necessary */
+		if (min_pos > i)
 		{
-			if (array[j] <= min_value)
-			{
-				min_value = array[j];
-				min_index = j;
-			}
-			if (j == (size - 1))
-			{
-				array[i] = min_value;
-				array[min_index] = start_value;
-			}
+			temp = arr[i];
+			arr[i] = arr[min_pos];
+			arr[min_pos] = temp;
+			/* print the array after swapping */
+			print_array(arr, size);
 		}
-
-		for(k = 0; k < size; k++)
-		{
-			printf("%d", array[k]);
-			if (k != size - 1)
-			{
-				printf(", ");
-			}
-		}
-		printf("\n");
 	}
 }
