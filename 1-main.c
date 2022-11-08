d3ee5643c03818f726f4ad8ad5704b0ab0d2616e
scaarifintl <scaarifintl@gmail.com>
2022-11-08 15:17:51 +0300

    implement shell sort

--- 1-main.c
+++ 1-main.c
@@ -39,8 +39,8 @@ int main(void)
 {
     listint_t *list;
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-    list = create_listint(array, n);
+    /*size_t n = sizeof(array) / sizeof(array[0]);*/
+    list = create_listint(array, 5);
     if (!list)
         return (1);
     print_list(list);
---
+++ 100-shell_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array in ascending order using shell sort
+ * Description: shell sort is an improvement of insertion sort.
+ * Reduces the number of swaps by increasing the interval an element can move
+ * to its rightful position (in insertion sort, an element moves a step a time)
+ * @array: the array to sort
+ * @size: the number of elements in the array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int interval = 1, temp, col, col_row;
+
+	/* calculate a knuth series interval: initialized to one at start */
+	while (interval <= (int)size / 3)
+		interval = interval * 3 + 1;
+	/* while interval is greater than 0 (interval > 0) */
+	while (interval > 0)
+	{
+	/* iterate over a column (elements at the same interval from each other) */
+		for (col = interval; col < (int)size; col++)
+		{
+	/* for a column; get last row element, [farthest index], save it in temp */
+			temp = array[col];
+			col_row = col; /* last row in a column */
+	/* while in column: (col_row >= interval && [col_row - interval] > temp */
+			while (col_row >= interval && array[col_row - interval] > temp)
+			{
+	/* update col_row's value to [col_row - interval]'s: to value just above it */
+				array[col_row] = array[col_row - interval];
+	/* adjust col_row to the row above it & repeat if True */
+				col_row -= interval; /* move up the column rows */
+			}
+	/* Regardless, (out of loop) adjust/update top col_row's value to temp */
+			array[col_row] = temp; /* this' the col's topmost row */
+		}
+	/* update interval (decrement it) & repeat while True */
+		interval = (interval - 1) / 3;
+		/* print array every time interval changes */
+		print_array(array, size);
+	}
+}
